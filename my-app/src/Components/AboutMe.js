392a12da13efb366f4dcfc2d2e72394deadba926
Lakshyasaharan5 <lakshyasaharan@gmail.com>
2022-11-25 18:05:07 +0530

    Updated About Me by adding social links.

--- my-app/src/Components/AboutMe.js
+++ my-app/src/Components/AboutMe.js
@@ -10,11 +10,11 @@ function Aboutme() {
             <div class="about-me-links">
                 <ul>
                     <li><SocialIcon url="https://github.com/Lakshyasaharan5" target="_blank" /></li>
-                    <li><SocialIcon url="https://www.linkedin.com/in/lakshya-saharan-8567611b1" target="_blank" /></li>
-                    <li><SocialIcon url="https://leetcode.com/lakshyasaharan" target="_blank" /></li>
+                    <li><SocialIcon url="https://www.linkedin.com/in/lakshya-saharan-8567611b1/" target="_blank" /></li>
+                    <li><SocialIcon url="https://www.leetcode.com/lakshyasaharan/" target="_blank" /></li>
                 </ul>
             </div>
-            <div class="about-me-intro"> Hi I am fast learner and can adopt based on company's requirement.</div>
+            <div class="about-me-intro"> I am fast learner and can adopt based on company's requirement. I have good knowledge of Java Springboot and MERN</div>
         </div>
     </section>
 }
